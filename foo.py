ec5196aa4510a00a5f62fbfc21365ece141a1a91
aitrusnc <aitrusnc@users.noreply.github.com>
2023-02-07 02:43:18 -0600

    Update foo.py

--- foo.py
+++ foo.py
@@ -1,20 +1,20 @@
 #! /usr/bin/python
 #-*-coding: utf-8 -*-
 # 2023-02-07:
-# 2022-12-21: 
-# 2020-01-19: 
+# 2022-12-21:
+# 2020-01-19:
 # calculates the area of squares, rectangles, or circles... and maybe triangles.
 
-def square(L):
-    return L * L
+def square(length):
+    return length * length
 
-def rectangle(width , height):
+def float(rectangle(width , height)):
     return width * height
 
 def circle(radius):
     return 3.14159 * radius ** 2
 
-def triangle(base , height):
+def float(triangle(base , height)):
     return base * height / 2
  
 def options():
@@ -28,29 +28,29 @@ def options():
     print()
  
 print("This program will calculate the area of a square, circle or rectangle.")
-choice = "x"
+Choice = "x"
 options()
-while choice != "q":
-    choice = input("Please enter your choice: ")
-    if choice == "s":
-        L = float(input("Length of square side: "))
-        print("The area of this square is", square(L))
+while Choice != "q":
+    Choice = input("Please enter your choice: ")
+    if Choice == "s":
+        length = float(input("Length of square side: "))
+        print("The area of this square is", square(l))
         options()
-    elif choice == "c":
+    elif Choice == "c":
         radius = float(input("Radius of the circle: "))
         print("The area of the circle is", circle(radius))
         options()
-    elif choice == "r":
+    elif Choice == "r":
         width = float(input("Width of the rectangle: "))
         height = float(input("Height of the rectangle: "))
         print("The area of the rectangle is", rectangle(width, height))
         options()
-    elif choice == "t":
+    elif Choice == "t":
         base = float(input("Base of the triangle: "))
         height = float(input("Height of the triangle: "))
         print("The area of the triangle is", triangle(base, height))
         options()
-    elif choice == "q":
+    elif Choice == "q":
         print(" ",end="")
     else:
         print("Unrecognized option.")
